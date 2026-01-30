91b63d063a238a3d704cb7ddcaf3cf6c5c7e38d5
anasmunawar2 <anasmunawar2@gmail.com>
2026-01-31 00:13:56 +0500

    feat: ai controllers updated

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -2,6 +2,9 @@ import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
 import axios from "axios";
+import { vs as cloudinary } from "cloudinary";
+
+import fs from "fs";
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -125,8 +128,8 @@ export const generateImage = async (req, res) => {
       });
     }
 
-    const form = new FormData();
-    form.append("prompt", prompt);
+    const formData = new FormData();
+    formData.append("prompt", prompt);
 
     const { data } = await axios.post(
       "https://clipdrop-api.co/text-to-image/v1",
@@ -141,19 +144,127 @@ export const generateImage = async (req, res) => {
 
     const base64Image = `data:image/png;base64,${Buffer.from(data, "binary").toString("base64")}`;
 
-    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+    const { secure_url } = await cloudinary.uploader.upload(base64Image);
 
-    if (plan !== "premium") {
-      await clerkClient.users.updateUserMetadata(userId, {
-        privateMetadata: {
-          free_usage: free_usage + 1,
-        },
-      });
-    }
+    await sql`INSERT INTO creations (user_id, prompt, content, type, publish) VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})`;
 
     res.json({
       success: true,
-      content,
+      content: secure_url,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const RemoveImageBackground = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { image } = req.file;
+    const plan = req.plan;
+
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available for premium subscriptions",
+      });
+    }
+
+    const { secure_url } = await cloudinary.uploader.upload(image.path, {
+      transitionProperty: [
+        {
+          effect: "background_removal",
+          background_removal: "remove_the_background",
+        },
+      ],
+    });
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, "Remove background from image", ${secure_url}, 'image')`;
+
+    res.json({
+      success: true,
+      content: secure_url,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const RemoveImageObject = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { object } = req.body;
+    const { image } = req.file;
+    const plan = req.plan;
+
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available for premium subscriptions",
+      });
+    }
+
+    const { public_id } = await cloudinary.uploader.upload(image.path);
+
+    const imageUrl = cloudinary.url(public_id, {
+      transitionProperty: [
+        {
+          effect: `gen_remove: ${object}`,
+        },
+      ],
+      resource_type: "image",
+    });
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${`Removed ${object} from image`}, ${imageUrl}, 'image')`;
+
+    res.json({
+      success: true,
+      content: imageUrl,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const resumeReview = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const resume = req.file;
+    const plan = req.plan;
+
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available for premium subscriptions",
+      });
+    }
+
+    if (resume.size > 5 * 1024 * 1024) {
+      return res.json({
+        success: false,
+        message: "Resume file size exceeds allowed size (5Mb).",
+      });
+    }
+
+    const dataBuffer = fs.readFileSync(resume.path);
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${`Removed ${object} from image`}, ${imageUrl}, 'image')`;
+
+    res.json({
+      success: true,
+      content: imageUrl,
     });
   } catch (error) {
     console.log(error.message);
