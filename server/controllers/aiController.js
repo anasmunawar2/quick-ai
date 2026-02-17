bb32a53f19f6b6fb7707ffe68ef890f1067d8bcc
anasmunawar2 <anasmunawar2@gmail.com>
2026-02-17 21:11:45 +0500

    feat: Remove Object API integrated

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -206,7 +206,7 @@ export const RemoveImageObject = async (req, res) => {
   try {
     const { userId } = req.auth();
     const { object } = req.body;
-    const { image } = req.file;
+    const image = req.file;
     const plan = req.plan;
 
     if (plan !== "premium") {
